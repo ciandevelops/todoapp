0e3883d66afed280625adb7e55a668829c4bbd69
ciandevelops <ciandevelops@gmail.com>
2022-05-03 22:30:15 -0400

    notes and footer styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import CreateArea from "./components/CreateArea";
 import Header from "./components/Header";
 import { useState } from "react";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -11,10 +13,30 @@ function App() {
       return [...prevNotes, newNote];
     });
   };
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
   return (
     <div className="App">
       <Header />
       <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
